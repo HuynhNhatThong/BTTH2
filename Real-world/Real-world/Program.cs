d17be20d9ef462533390a45c0dc3b66fc9d88fa2
HuynhNhatThong <thong_dpm225486@student.agu.edu.vn>
2025-10-06 08:02:23 +0700

    Add files via upload

--- Real-world/Real-world/Program.cs
+++ Real-world/Real-world/Program.cs
@@ -6,27 +6,22 @@ using System.Threading.Tasks;
 
 namespace Real_world
 {
-    public class MainApp
+    class MainApp
     {
         /// <summary>
         /// Entry point into console application.
         /// </summary>
         public static void Main()
         {
-            VehicleBuilder builder;
-            // Create shop with vehicle builders
-            Shop shop = new Shop();
-            // Construct and display vehicles
-            builder = new ScooterBuilder();
-            shop.Construct(builder);
-            builder.Vehicle.Show();
-            builder = new CarBuilder();
-            shop.Construct(builder);
-            builder.Vehicle.Show();
-            builder = new MotorCycleBuilder();
-            shop.Construct(builder);
-            builder.Vehicle.Show();
-            // Wait for user
+            // Create and run the African animal world
+            ContinentFactory africa = new AfricaFactory();
+            AnimalWorld world = new AnimalWorld(africa);
+            world.RunFoodChain();
+            // Create and run the American animal world
+            ContinentFactory america = new AmericaFactory();
+            world = new AnimalWorld(america);
+            world.RunFoodChain();
+            // Wait for user input
             Console.ReadKey();
         }
     }
